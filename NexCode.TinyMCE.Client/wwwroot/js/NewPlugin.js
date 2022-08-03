506455ac810a9c56f5ce47c00d40a25263157d1b
Chris Jackson <chrisjackson92@gmail.com>
2022-08-03 15:55:25 +0100

    add example plugin

--- NexCode.TinyMCE.Client/wwwroot/js/NewPlugin.js
+++ NexCode.TinyMCE.Client/wwwroot/js/NewPlugin.js
@@ -1,1 +1,49 @@
 ﻿
+
+export function showDialog(editorId, tokens) {
+    var editor = tinymce.get(editorId);
+
+    editor.windowManager.open({
+        title: 'Example plugin',
+        body: {
+            type: 'panel',
+            items: [
+                {
+                    type: 'grid',
+                    columns: 3,
+                    items: [
+                        {
+                            type: 'button',
+                            text: 'title',
+                            name:'value'
+                        },
+                        {
+                            type: 'button',
+                            text: 'title',
+                        },
+                        {
+                            type: 'button',
+                            text: 'title',
+                        }
+
+                    ]
+                }
+            ]
+        },
+        buttons: [
+            {
+                type: 'submit',
+                text: 'Close'
+            }
+        ],
+        onAction: function(api, d, t, y) {
+
+        },
+        onSubmit: function (api) {
+            var data = api.getData();
+            // Insert content when the window fsorm is submitted #1#
+            editor.insertContent('Title: ' + data.title);
+            api.close();
+        }
+    });
+}
